7cd88efd27ac943b27bf58dd16f3b93a0aa0b295
tomnaZ1 <boss_tomna@hotmail.com>
2020-06-08 00:53:09 +0200

    Product details pt2

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ProductRoutingModule } from './product-routing.module';
-import { ProductComponent } from './product.component';
-
+import { ProductRoutingModule } from "./product-routing.module";
+import { ProductComponent } from "./product.component";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [ProductComponent],
-  imports: [
-    CommonModule,
-    ProductRoutingModule
-  ]
+  imports: [CommonModule, ProductRoutingModule, MatSelectModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
-export class ProductModule { }
+export class ProductModule {}
