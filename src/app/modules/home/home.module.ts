5937354a2c2e95ece16ebca2d73a4d451a9b0f85
Waleed Rostom <waleedrostom@gmail.com>
2020-06-08 02:13:56 +0200

    home pt1

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
 
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, MDBBootstrapModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
