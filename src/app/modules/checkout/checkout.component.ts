9b24d7453eecb7800587933b25d0e8ba7d3870b1
tomnaZ1 <boss_tomna@hotmail.com>
2020-06-09 01:54:34 +0200

    Checkout pt1

--- src/app/modules/checkout/checkout.component.ts
+++ src/app/modules/checkout/checkout.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  selector: "app-checkout",
+  templateUrl: "./checkout.component.html",
+  styleUrls: ["./checkout.component.scss"],
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  isEditable = false;
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ["", Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ["", Validators.required],
+    });
   }
-
 }
--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { CheckoutRoutingModule } from './checkout-routing.module';
-import { CheckoutComponent } from './checkout.component';
-
+import { CheckoutRoutingModule } from "./checkout-routing.module";
+import { CheckoutComponent } from "./checkout.component";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  declarations: [CheckoutComponent],
+  declarations: [CheckoutComponent, ProductCardComponent],
   imports: [
     CommonModule,
-    CheckoutRoutingModule
-  ]
+    CheckoutRoutingModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCardModule,
+  ],
 })
-export class CheckoutModule { }
+export class CheckoutModule {}
