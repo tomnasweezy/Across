417cf2f54ef380a6cb3e0f8ae9dfbe7ad6acfafd
tomnaZ1 <boss_tomna@hotmail.com>
2020-06-16 20:42:54 +0200

    created about us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: "product/:productId", loadChildren: () => import("./modules/product/product.module").then((m) => m.ProductModule) },
   { path: "checkout", loadChildren: () => import("./modules/checkout/checkout.module").then((m) => m.CheckoutModule) },
   { path: 'contactus', loadChildren: () => import('./modules/contactus/contactus.module').then(m => m.ContactusModule) },
+  { path: 'aboutus', loadChildren: () => import('./modules/aboutus/aboutus.module').then(m => m.AboutusModule) },
 ];
 
 @NgModule({
