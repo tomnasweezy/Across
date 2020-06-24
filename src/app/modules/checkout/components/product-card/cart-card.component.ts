94e20daed80d8787d425840626ab40a7353dd293
Waleed Rostom <waleedrostom@gmail.com>
2020-06-24 23:49:41 +0200

    update image in cart

--- src/app/modules/checkout/components/product-card/cart-card.component.ts
+++ src/app/modules/checkout/components/product-card/cart-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, ElementRef, EventEmitter, Input } from "@angular/core";
 import { CartItem } from "src/app/shared/models/cart.model";
 import { Product } from "src/app/shared/models/product.model";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-cart-card",
@@ -11,6 +12,7 @@ export class ProductCardComponent implements OnInit {
   @Input("productData") data: CartItem;
   @Output("delete") deleteItem = new EventEmitter();
   @Output("quantity") updateQuantity = new EventEmitter();
+  imgUrlPrefix: string = `${environment.mediaUrl}:${environment.port}/`;
   product: Product;
   constructor() {}
 
