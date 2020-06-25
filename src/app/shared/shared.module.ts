a0d81bc718d66357f8799437e0e171358b560788
tomnaZ1 <boss_tomna@hotmail.com>
2020-06-25 23:17:34 +0200

    error dialog component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoadingSpinnerComponent } from "./components/loading-spinner/loading-spinner.component";
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 
 @NgModule({
-  declarations: [LoadingSpinnerComponent],
+  declarations: [LoadingSpinnerComponent, ErrorDialogComponent],
   imports: [CommonModule],
   exports: [LoadingSpinnerComponent],
 })
