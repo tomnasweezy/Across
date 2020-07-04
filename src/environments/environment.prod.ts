90e8b8268d28e983825fd334f60a712cbf643f4b
tomnaZ1 <boss_tomna@hotmail.com>
2020-07-04 18:40:34 +0200

    addjusting price + environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: "http://localhost",
-  mediaUrl: "http://localhost",
+  apiUrl: "http://api.acrosseghisham.com",
+  mediaUrl: "http://api.acrosseghisham.com",
   port: 4000,
   webport: 4200,
   production: true,
