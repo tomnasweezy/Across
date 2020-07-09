ed0c5446184c6935d41fd0b5a76f531bd0705f0c
tomnaZ1 <boss_tomna@hotmail.com>
2020-07-09 21:03:47 +0200

    update envi pt2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: "http://localhost",
-  mediaUrl: "http://localhost",
+  apiUrl: "http://178.79.184.211",
+  mediaUrl: "http://178.79.184.211",
   port: 4000,
   webport: 4200,
   production: true,
